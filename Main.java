a1692ae9df325cf71d0a30014072732c80f6c717
Arein Gupta <76914288+areingupta@users.noreply.github.com>
2022-01-01 21:58:46 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] numbers = { 2,3,4,5,6 };
+        Arrays.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
+    }
+}
