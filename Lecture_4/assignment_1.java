57087dee0a81dc80556f3651978ca8f7b5be69d3
Arein Gupta <76914288+areingupta@users.noreply.github.com>
2022-01-01 22:10:02 +0530

    Add files via upload

---
+++ Lecture_4/assignment_1.java
@@ -0,0 +1,12 @@
+package assignmnent_questions;
+
+import java.util.Scanner;
+
+public class assignment_1 {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int i = 1;
+
+    }
+}
